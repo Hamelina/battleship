4f6c26ea1630dc5c399dc56c0f37a83960aecc6b
Hamelina <ehamelojulia@gmail.com>
2018-10-04 12:46:23 +0200

    I/O handled

--- src/main/scala/AIvsAI.scala
+++ src/main/scala/AIvsAI.scala
@@ -1,33 +1,8 @@
 
-
+//TODO
 /**
-  * This is a programm that gives proof of the assertion
+  * This is a programm that gives proof of the assertion concerning the AIs
   */
 case class AIvsAI() {
 
 }
-//to transform in grid
-/*def initializeGridFromFleet(fleet: List[Ship], size: Int): List[List[String]] = {
-  val emptyGrid: List[List[String]] = List(List(""))
-  val cells: List[Cell] = (fleet.map( x => x.cells)).flatMap(x => x)
-  createStatusLists(cells = cells, line = 0, statusList = emptyGrid)
-
-  @tailrec
-  def createStatusLists(cells: List[Cell], line: Int, statusList: List[List[String]]): List[List[String]] = {
-
-    if (line >= size - 1 && statusList.size >= size-1) {
-      statusList
-    }
-    else{
-      createStatusLists(cells, line+1, statusList = statusList:+fillListOfLines(line+1, 0, size, cells, Nil))
-    }
-    @tailrec
-    def fillListOfLines(line: Int, column: Int, size: Int, cells: List[Cell], list: List[String]): List[String] = {
-      if (column <= size-1 && cells.contains(Cell(line, size))) {
-        fillListOfLines(line, column + 1, size, cells, list = list :+ Utility.OCCUPIED_STATUS)
-      }
-      else if (!cells.contains(Cell(line, size))) {fillListOfLines(line, column + 1, size, cells, list = list :+ Utility.NOT_TARGETED)}
-      else list
-    }
-  }
-}*/
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,11 @@
 import game._
+import iohandler._
 import player._
 
 object Main extends App {
 
 
-  println("Welcome to you ! You have started the most enjoyable game ever: the battleship !")
+  Display.printWelcome
 
   gameBuilder()
 
@@ -28,24 +29,21 @@ object Main extends App {
         mainLoop(None, player1, player2, Game.NUMBER_OF_TOTAL_ROUND)
       }
         //add here the little program that runs AI vs AI
-      case _ => println("Something went wrong. The source code must have been changed")
+      case _ => Display.printSomethingWentWrong
     }
 
-//TODO changer les println
+
   def mainLoop(gameState: Option[Game], looser: Player, winner: Player, nbRoundsToPlay: Int): Unit = {
     Utility.clearScreen()
     if (nbRoundsToPlay == 0) {
       println("Game finished")
       //TODO implement writeResultsIntoCsvFile function in game
       //gameState.getOrElse(Game(looser, winner).writeResultsIntoCsvFile("aiproof")
-      println("Score: " + winner.name + ": " + winner.score + " VS " + looser.name + ": " + looser.score)
+      Display.printScore(winner.name, winner.score, looser.name, looser.score)
     }
     else {
       val util: Utility = Utility(gameState.getOrElse(Utility.initializeRound(looser: Player, winner: Player)))
-      /*val util: Utility = gameState match {
-        case None => Utility(Utility.initializeRound(looser: Player, winner: Player).get)
-        case _ => Utility(gameState.get)
-      }*/
+
       //Ask the players to shoot
       val currentGameState: Game = util.askPlayerToShoot()
 
@@ -53,14 +51,14 @@ object Main extends App {
       if (currentGameState.isGameOver) {
         if (currentGameState.player1.hasLost) {
           val winner: Player = currentGameState.player1.copy(_score = currentGameState.player1.score + 1)
-          println("Congratulations to " + winner.name + ", you won this round ! ")
-          println("Score: " + winner.name + ": " + winner.score + " VS " + currentGameState.player2.name + ": " + currentGameState.player2.score)
+          Display.printVictory(winner.name)
+          Display.printScore(winner.name, winner.score, currentGameState.player2.name, currentGameState.player2.score)
           mainLoop(None, currentGameState.player2, winner, nbRoundsToPlay - 1)
         }
         else {
           val winner = currentGameState.player2.copy(_score = currentGameState.player2.score + 1)
-          println("Congratulations to " + winner.name + ", you won this round ! ")
-          println("Score: " + winner.name + ": " + winner.score + " VS " + currentGameState.player1.name + ": " + currentGameState.player1.score)
+          Display.printWinner(winner.name)
+          Display.printScore(winner.name, winner.score, currentGameState.player1.name, currentGameState.player1.score)
           mainLoop(None, currentGameState.player2, winner, nbRoundsToPlay - 1)
         }
       }
@@ -70,106 +68,3 @@ object Main extends App {
     }
   }
 }
-
-
-
-
-
-
-
-  /**
-    * create game with player1 and player 2
-    * val gameRecord = Nil
-    *
-    * mainloop(Option[game], gameRecords)
-    *
-    * mainLoop(gameState: Game, gameRecords)
-    *   console.clear
-    *   if(game){
-      *   if gameState.player1.hasLost || gameState.player2.hasLost (end of game)
-      *     printGameOver
-      *     continue : ReadLine (Do you still want to play ? )
-      *     val record: GameRecords;
-      *     if gameState.player1.hasLost{
-      *       record = Game(gameState.player2.copy(score=player2.score+1), gameState.player1) +: gamerecord
-      *       }
-      *     else {
-      *       record = Game(player1.copy(score=player1.score+1), gameState.player2) +: gamerecord
-      *     }
-      *     if (continue){
-      *       mainLoop(None, record)
-      *     }
-      *     else prinln("Partie over", player1.name won .../...)
-      *  else{
-    *       game.renderGridToUser
-      *     val newGame: Game = Utility.askUserForShoot(gameState)
-    *       Console.clear
-    *       renderGridToUser
-    *       mainloop(newGame, gameRecords)
-    *     }
-    *
-    *
-    *   }
-    */
-
-
-
-
-
-  /*Init 2 grid
-  *
-  * */
-
-
-  /*TO DO: lui demander de rentrer les positions de chaque bateau:
-  *
-  * */
-
-
-  //Utility.askUserForShipSettings(Utility.NUMBER_AND_SIZE_OF_SHIPS, Nil,"Bill")
-  //var ship = Utility.askUserForShipSettings(3, Nil)
-  //var grid = initializeGridFromFleet(ship, 10)
-  //println(grid)
-
-  //createGridFromFleet(ship)
-
-/*println("You have the possibility to play either against another player or against 3 levels of Artificial Intelligence. If you want to play against: ")
-  println("another player --> press 0.")
-  println("AI level 1 --> press 1.")
-  println("AI level 2 --> press 2." )
-  println("AI level 3 --> press 3.")
-  val input = scala.io.StdIn.readLine("Please enter your game mode: ")
-  input match {
-    case "0" => println("You decide to play against another player")
-    case "1" => println("You decide to play against AI level 1 ")
-    case "2" => println("You decide to play against AI level 2")
-    case "3" => println("You decide to play against AI level 3")
-    case _ => println("Your input was not expected, sorry")
-  }
-  val player1Name = scala.io.StdIn.readLine("Please enter the name of the first player who will start the game: ")
-  val player2Name = scala.io.StdIn.readLine("Please enter the name of the other player: ")
-
-  println("----Player 1: "+player1Name+"********* Player 2:"+player2Name)*/
-
-/*
-  /*val player1Name = "Player 1"
-  val player2Name = "Player 2"
-
-  val levelPlayer1: Option[Int] = None
-  val levelPlayer2: Option[Int] = None
-
-
-
-
-  val grid: Grid = Grid(Nil)
- // private val _fleet: List[Ship], private val _name: String, private val _level: Option[Int], private val _score: Int, private val _isTurnToPlay: Boolean, private val _gridStates: Grid
-  val player1 = Player(Nil, player1Name, levelPlayer1, 0, true, Grid(Nil))
-  println("******* PLAYER 1 CREATE *******")
-  val player2 = Player(Nil, player2Name, levelPlayer1, 0, false, Grid(Nil))
-  println("******* PLAYER 2 CREATE *******")
-  println(player2)
-  val round = Game(player1, player2)
-  println(player1)
-  val nb=1
-  mainLoop(None, this.player1, this.player2, nb)*/
- */
